0fffa17d810d05ab6bd6d38f7c2429cd7a955428
Jacob Dearing <jacob.dearing@gmail.com>
2012-09-22 22:41:34 -0500

    add generic cross compile script for go in ruby

---
+++ tools/go-crosscompile.rb
@@ -0,0 +1,34 @@
+
+systems = ['windows','linux', 'netbsd','freebsd','openbsd','darwin']
+archs 	= ['386','amd64','arm']
+
+if ARGV.length < 3
+	puts "requires [CMD] [OS] [ARCH]"
+	puts "where [CMD] is within GOBIN dir"
+	puts "where [OS] is of #{systems}"
+	puts "where [ARCH] is of #{archs}"
+	puts "for example: go-crosscompile build linux arm"
+	exit
+end
+
+cmd = ARGV[0]
+
+if systems.include? ARGV[1]
+	os = ARGV[1]
+else
+	puts "#{ARGV[1]} not of systems #{systems}"
+	exit
+end
+
+if archs.include? ARGV[2]
+	arch = ARGV[2]
+else
+	puts "#{ARGV[2]} not of architectures #{archs}"
+	exit
+end
+
+ENV["CGO_ENABLED"] = "0"
+ENV["GOOS"] = os
+ENV["GOARCH"] = arch
+
+`go #{cmd}`
