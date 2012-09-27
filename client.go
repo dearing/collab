393300b7a2114fe3da0e783180b74a93c386e57e
Jacob Dearing <jacob.dearing@gmail.com>
2012-09-26 21:14:23 -0500

    consolidate client logic into one source

--- client.go
+++ client.go
@@ -1,7 +1,5 @@
 package main
 
-// JSON format of a client intent, payload and origin
-
 import (
 	"code.google.com/p/go.net/websocket"
 	. "fmt"
@@ -13,12 +11,12 @@ type Client struct {
 	Lock   bool
 }
 
+// JSON format of a client intent, payload and origin
 type ClientJSON struct {
 	Action string // an intent
 	Data   string // payload of intent
 	Origin string // origin of intent
 }
-
 
 func (client *Client) mesg(action, data, origin string) *Message {
 	return &Message{Client: client, JSON: &ClientJSON{Action: action, Data: data, Origin: origin}}
@@ -28,23 +26,32 @@ func echo(action, data, origin string) *ClientJSON {
 	return &ClientJSON{Action: action, Data: data, Origin: origin}
 }
 
+func WebsocketHandler(ws *websocket.Conn) {
 
+	client := &Client{
+		Name:   "Client",
+		Socket: ws,
+	}
 
-func (client *Client) Loop() {
+	// Here we wait for the client to provide us a nickname in Data
+	// so we can avoid throwing out generic usernames.  There is no
+	// other way for us to know how the client wants to be indentified
+	// without storing information at the server; which we do not want.
 
 	var q ClientJSON
-
-	defer func() {
-		Router.Remove <- client
-		Router.Broadcast <- client.mesg("inform", Sprintf("%s disconnected.", client.Name), "Server")
-	}()
-
 	if err := websocket.JSON.Receive(client.Socket, &q); err != nil {
 		return
 	}
 
 	client.Name = q.Data
+
+	Router.Add <- client
 	Router.Echo <- echo("inform", Sprintf("%s connected.", client.Name), "Server")
+
+	defer func() {
+		Router.Remove <- client
+		Router.Broadcast <- client.mesg("inform", Sprintf("%s disconnected.", client.Name), "Server")
+	}()
 
 	// gnab the first copy of the text we can find (that is not our own)
 	for peer := range Router.Clients {
@@ -54,6 +61,7 @@ func (client *Client) Loop() {
 		}
 	}
 
+	// ForEhVer: http://www.youtube.com/watch?v=H-Q7b-vHY3Q
 	for {
 
 		var q ClientJSON
@@ -70,7 +78,7 @@ func (client *Client) Loop() {
 			return
 
 		case "speech":
-			Router.Echo <- echo("speech",q.Data,client.Name)
+			Router.Echo <- echo("speech", q.Data, client.Name)
 		case "lock":
 			client.Lock = true
 			Router.Broadcast <- client.mesg("lock", q.Data, client.Name)
